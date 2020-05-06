ac50bb30cfc1d5059b8cb8ff53310c559a066c4e
Ynng <44907675+Ynng@users.noreply.github.com>
2020-05-06 17:36:39 -0400

    done

--- Keep2txt.py
+++ Keep2txt.py
@@ -7,47 +7,36 @@ import sys
 import os
 
 stdout_fileno = sys.stdout
-sys.stdout = open('output.json',"w")
+sys.stdout = open('output.json', "w")
+
 
 def randomString(stringLength=32):
     letters = string.ascii_lowercase + string.digits
     return ''.join(random.choice(letters) for i in range(stringLength))
 
-pathlist = Path("D:/User/Downloads/Takeout/Keep").glob("*.json")
-output = {"activeNotes": [], "trashedNotes":[]}
+
+pathlist = Path("D:/User/Downloads/Test").glob("*")
+output = {"activeNotes": [], "trashedNotes": []}
 
 for path in pathlist:
-  path_in_str = str(path)
+    path_in_str = str(path)
+    with open(path_in_str+"\\info.json", encoding='utf-8') as fh:
+        data = json.load(fh)
 
-  with open(path_in_str, encoding='utf-8') as fh:
-      data = json.load(fh)
 
-  timestamp = data["userEditedTimestampUsec"]/1000000
-  dt_object = datetime.utcfromtimestamp(timestamp)
-  dateString = "{:%Y-%m-%dT%H:%M:%S.000Z}".format(dt_object)
+    timestamp = data["noteEntity"]["creationDate"]/1000
+    dt_object = datetime.fromtimestamp(timestamp)
+    creationDate = "{:%Y-%m-%dT%H:%M:%S.000Z}".format(dt_object)
+    timestamp = data["noteEntity"]["modificationDate"]/1000
+    dt_object = datetime.fromtimestamp(timestamp)
+    modificationDate = "{:%Y-%m-%dT%H:%M:%S.000Z}".format(dt_object)
 
-  textContent = ""
-  if 'textContent' in data:
-    textContent = data['textContent']
-  elif 'listContent' in data:
-    textContent = ""
-    for item in data['listContent']:
-      if(item["isChecked"]):
-        textContent = textContent + "- [x] "
-      else:
-        textContent = textContent + "- [ ] "
+    with open(path_in_str+"\\text.markdown", encoding='utf-8') as fh:
+      content = fh.read()
+      
+    # print(content)
 
-      textContent = textContent + item["text"] + "\n"
-
-  if not data['title']:
-    content = textContent
-  else: 
-    content = data['title'] + "\n" + textContent
-
-  noteId = randomString(32)
-  if (not data["isArchived"]) and (not data["isTrashed"]):
-    output["activeNotes"].append({"id": noteId, "content": content, "creationDate" : dateString, "lastModified": dateString})
-  else:
-    output["trashedNotes"].append({"id": noteId, "content": content, "creationDate" : dateString, "lastModified": dateString})
+    noteId = data["noteEntity"]["identifier"]
+    output["activeNotes"].append({"id": noteId, "content": content, "creationDate" : creationDate, "lastModified": modificationDate})
 
 sys.stdout.write(json.dumps(output))

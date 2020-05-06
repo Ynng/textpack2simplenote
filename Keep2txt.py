ea5b2f3b2ee6d47aa05570cb9e35ed4d20c26190
Ynng <44907675+Ynng@users.noreply.github.com>
2020-05-06 17:50:34 -0400

    finished

--- Keep2txt.py
+++ Keep2txt.py
@@ -15,7 +15,7 @@ def randomString(stringLength=32):
     return ''.join(random.choice(letters) for i in range(stringLength))
 
 
-pathlist = Path("D:/User/Downloads/Test").glob("*")
+pathlist = Path("D:/User/Downloads/Backup").glob("*")
 output = {"activeNotes": [], "trashedNotes": []}
 
 for path in pathlist:
@@ -23,19 +23,27 @@ for path in pathlist:
     with open(path_in_str+"\\info.json", encoding='utf-8') as fh:
         data = json.load(fh)
 
-
-    timestamp = data["noteEntity"]["creationDate"]/1000
-    dt_object = datetime.fromtimestamp(timestamp)
-    creationDate = "{:%Y-%m-%dT%H:%M:%S.000Z}".format(dt_object)
     timestamp = data["noteEntity"]["modificationDate"]/1000
     dt_object = datetime.fromtimestamp(timestamp)
     modificationDate = "{:%Y-%m-%dT%H:%M:%S.000Z}".format(dt_object)
+    timestamp = data["noteEntity"]["creationDate"]/1000
+    dt_object = datetime.fromtimestamp(timestamp)
+    creationDate = "{:%Y-%m-%dT%H:%M:%S.000Z}".format(dt_object)
+
 
     with open(path_in_str+"\\text.markdown", encoding='utf-8') as fh:
       content = fh.read()
-      
-    # print(content)
+    
+    content_list = content.split("\n")
+    if content_list[0].startswith("#") and content_list[1].startswith("201"):
+      content_list.append("")
+      content_list.append(content_list[0])
+      del content_list[0]
+    
+    if not content_list[0].startswith("201"):
+      content_list.insert(0, "{:%Y/%m/%d}".format(dt_object))
 
+    content="\n".join(content_list)
     noteId = data["noteEntity"]["identifier"]
     output["activeNotes"].append({"id": noteId, "content": content, "creationDate" : creationDate, "lastModified": modificationDate})
 
